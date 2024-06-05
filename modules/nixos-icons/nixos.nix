966cb3c7dff598f39cdd5b8049508184f7dd54cb
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2024-06-05 22:53:16 +0530

    Update nixos.nix

--- modules/nixos-icons/nixos.nix
+++ modules/nixos-icons/nixos.nix
@@ -4,20 +4,21 @@
   options.stylix.targets.nixos-icons.enable =
     config.lib.stylix.mkEnableTarget "the NixOS logo" true;
 
-  config.nixpkgs.overlays = lib.mkIf config.stylix.targets.nixos-icons.enable [(self: super: {
-    nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
-      src = pkgs.applyPatches {
-        src = oldAttrs.src;
-        prePatch = ''
-          substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${config.lib.stylix.colors.base05}'
-
-          # Insert attribution comment after the XML prolog
-          sed \
-            --in-place \
-            '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${config.lib.stylix.colors.scheme} color scheme. -->' \
-            logo/nix-snowflake-white.svg
-      '';
-      };
-    });
-  })];
+  config.test = lib.mkIf config.stylix.targets.nixos-icons.enable "Test";
+#[(self: super: {
+#    nixos-icons = super.nixos-icons.overrideAttrs (oldAttrs: {
+#      src = pkgs.applyPatches {
+#        src = oldAttrs.src;
+#        prePatch = ''
+#          substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${config.lib.stylix.colors.base05}'
+#
+#          # Insert attribution comment after the XML prolog
+#          sed \
+#            --in-place \
+#            '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${config.lib.stylix.colors.scheme} color scheme. -->' \
+#            logo/nix-snowflake-white.svg
+#      '';
+#      };
+#    });
+#  })];
 }
