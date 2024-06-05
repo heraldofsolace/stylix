68d21883e9da129a9b3ba8c6153021e6566ab2b5
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2024-06-05 22:47:27 +0530

    Update nixos.nix

--- modules/nixos-icons/nixos.nix
+++ modules/nixos-icons/nixos.nix
@@ -9,12 +9,12 @@
       src = pkgs.applyPatches {
         src = oldAttrs.src;
         prePatch = ''
-          substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${base05}'
+          substituteInPlace logo/nix-snowflake-white.svg --replace-fail '#ffffff' '#${config.lib.stylix.colors.base05}'
 
           # Insert attribution comment after the XML prolog
           sed \
             --in-place \
-            '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${scheme} color scheme. -->' \
+            '2i<!-- The original NixOS logo from ${oldAttrs.src.url} is licensed under https://creativecommons.org/licenses/by/4.0 and has been modified to match the ${config.lib.stylix.colors.scheme} color scheme. -->' \
             logo/nix-snowflake-white.svg
       '';
       };
